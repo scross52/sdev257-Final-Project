2a645fce7618c6065921f16cb9c6403d4998ba10
dellis2301 <dellis75@ivytech.edu>
2025-12-16 14:56:08 -0500

    Add movie search functionality using TMDB API

---
+++ services/tmdb.js
@@ -0,0 +1,10 @@
+const API_KEY = "87a2cba59c043feed9dd9007377bbc3f";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+  );
+  const data = await response.json();
+  return data.results;
+};
